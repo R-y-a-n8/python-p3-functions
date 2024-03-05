7305eeaa846bcfbd76c1d82e90b6af3e34d8ed23
devRyan <ryan.kinuthia@student.moringaschool.com>
2024-03-05 13:10:06 +0300

    halv3

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print("Hello, {}!".format(name))
+
+greet("Alice")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print("Hello, {}!".format(name))
+
+greet_with_default("James")
+
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 + num2 
+
+result = add(45, 55)
+print(result)
 
 def halve(number):
-    pass
+    return number / 2
+
+result = halve(10)
+print(result)
